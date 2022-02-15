fca6ac226481461c032921e114685c0e8c6440bb
alerebal <ale.rebal@gmail.com>
2022-02-15 08:11:02 +0000

    Add static folder and tasks.html

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,15 +1,14 @@
-import imp
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env # noqa
+    import env
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
-from taskmanager import routes # noqa
+from taskmanager import routes
