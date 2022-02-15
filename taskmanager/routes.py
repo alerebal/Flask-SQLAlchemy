27507cb6f0998f6181459ba469d2692030d6c8c7
alerebal <ale.rebal@gmail.com>
2022-02-15 10:09:47 +0000

    Add categories and add category views and routing them

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from crypt import methods
+from unicodedata import category
+from flask import redirect, render_template, request, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -6,3 +8,18 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template('tasks.html')
+
+
+@app.route('/categories')
+def categories():
+    return render_template('categories.html')
+
+
+@app.route('/add_category', methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(catagory_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for('categories'))
+    return render_template('add_category.html')
