3635c280e31fbd395a5b825b37f149be3db1af9d
alerebal <ale.rebal@gmail.com>
2022-02-15 10:53:33 +0000

    Add list of categories to categories.html

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,13 +4,13 @@ from taskmanager import db
 class Category(db.Model):
     # scherma for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    catagory_name = db.Column(db.String(25), unique=True, nullable=False)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return self.catagory_name
+        return self.category_name
 
 
 class Task(db.Model):
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,5 @@
 from crypt import methods
+from time import clock_getres
 from unicodedata import category
 from flask import redirect, render_template, request, url_for
 from taskmanager import app, db
@@ -12,13 +13,15 @@ def home():
 
 @app.route('/categories')
 def categories():
-    return render_template('categories.html')
+    categories = list(Category.query.order_by(Category.category_name).all())
+    print(categories)
+    return render_template('categories.html', categories=categories)
 
 
 @app.route('/add_category', methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
-        category = Category(catagory_name=request.form.get("category_name"))
+        category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
         return redirect(url_for('categories'))
