afafafe67be7621c1fe156da0adc37b28d6eeca8
alerebal <ale.rebal@gmail.com>
2022-02-16 09:05:28 +0000

    Add update functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,3 @@
-from crypt import methods
-from time import clock_getres
-from unicodedata import category
 from flask import redirect, render_template, request, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
@@ -26,3 +23,13 @@ def add_category():
         db.session.commit()
         return redirect(url_for('categories'))
     return render_template('add_category.html')
+
+
+@app.route('/edit_category/<int:category_id>', methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template('edit_category.html', category=category)
