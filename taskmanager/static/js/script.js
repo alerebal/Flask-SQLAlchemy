e546b43606191741582593d0802fd41788b23e21
alerebal <ale.rebal@gmail.com>
2022-02-17 12:26:32 +0000

    Add add_task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,7 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
     // modal delete_category
     let elems = document.querySelectorAll('.modal');
     M.Modal.init(elems);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
