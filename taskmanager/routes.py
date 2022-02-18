3d26bcc3153a0dbb5608a9c9182a6896ed4cb400
alerebal <ale.rebal@gmail.com>
2022-02-18 11:52:40 +0000

    Add edit task functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import redirect, render_template, request, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
@@ -43,12 +44,6 @@ def delete_category(category_id):
     return redirect(url_for('categories'))
 
 
-# @app.route('/tasks')
-# def tasks():
-#     tasks = list(Task.query.order_by(Task.task_name).all())
-#     return render_template('tasks.html', tasks=tasks)
-
-
 @app.route('/add_task', methods=["GET", "POST"])
 def add_task():
     categories = list(Category.query.order_by(Category.category_name).all())
@@ -64,3 +59,21 @@ def add_task():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('add_task.html', categories=categories)
+
+
+@app.route('/edit_task/<int:task_id>', methods=["GET", "POST"])
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task.task_name = request.form.get('task_name')
+        task.task_description = request.form.get('task_description')
+        task.is_urgent = bool(True if request.form.get('is_urgent') else False)
+        task.due_date = request.form.get('due_date')
+        task.category_id = request.form.get('category_id')
+    
+        db.session.commit()
+        
+        return redirect(url_for('home'))
+        
+    return render_template('edit_task.html', task=task, categories=categories)
